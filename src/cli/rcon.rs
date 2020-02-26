69c4765e2a21b221454a1b2712cce152f0b1de06
Scott Hussey <sthussey@gmail.com>
2020-02-25 20:50:12 -0600

    [config] Add toml parsing code
    TOML seems to be a sane choice for describing
    a run context. Add needed crate dependencies
    and start with some test parsing code

--- src/cli/rcon.rs
+++ src/cli/rcon.rs
@@ -1,3 +1,22 @@
+use serde_derive::Deserialize;
+use toml::de::Error;
+
+#[derive(Deserialize)]
+struct Config {
+    run: String,
+    wd: Option<String>,
+}
+
 fn main() {
-    println!("Hello, World!");
+    let config_parse = parse_config(b"run = '/test'\nwd = '/tmp'\n");
+
+    match config_parse {
+        Ok(config) => println!("Running {}", config.run),
+        Err(err) => println!("Error parsing config: {}", err),
+    };
+}
+
+fn parse_config(cfg: &[u8]) -> Result<Config, Error> {
+    let config = toml::from_slice(cfg);
+    return config;
 }
