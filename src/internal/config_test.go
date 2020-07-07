f6aef8924b0512d081ae1d6490bad4944b144c0c
Scott Hussey <sthussey@gmail.com>
2020-07-06 20:55:42 -0500

    Refactor in Go
    - Go originally was dismissed due to my understanding of the goroutine
      to OS process mapping. However I've learned additional runtime
      functions exist to allow for predictable control of the underlying
      OS process. And Rust is nice, but the borrow checker is just too
      painful for toy code.

---
+++ src/internal/config_test.go
@@ -0,0 +1,52 @@
+package internal
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestJsonLoad(t *testing.T) {
+	runpath := getContextValues("RunPath")
+	workingdir := getContextValues("WorkingDir")
+	sample := []byte(fmt.Sprintf("{\"runpath\": \"%s\", \"workingdir\": \"%s\"}", runpath, workingdir))
+	result, err := jsonParseConfig(sample)
+	if err != nil {
+		t.Errorf("Valid JSON Load Failed: %v", err)
+	}
+	validateContext(t, result)
+}
+
+func TestEnvLoad(t *testing.T) {
+	sample := make(map[string]string)
+	sample["RCON_RUNPATH"] = getContextValues("RunPath")
+
+	sample["RCON_WORKINGDIR"] = "/tmp"
+	result, err := envParseConfig(sample)
+	if err != nil {
+		t.Errorf("Valid environment load failed: %v", err)
+	}
+	validateContext(t, result)
+}
+
+/* Reusable source for building test contexts
+   k is a key to get a value for
+*/
+func getContextValues(k string) string {
+	switch k {
+	case "RunPath":
+		return "/bin/false"
+	case "WorkingDir":
+		return "/tmp"
+	default:
+		return ""
+	}
+}
+
+func validateContext(t *testing.T, pctx *ProcessContext) {
+	if pctx.RunPath != getContextValues("RunPath") {
+		t.Errorf("JSON Load - Context has wrong RunPath - Found %s, expected %s", pctx.RunPath, getContextValues("RunPath"))
+	}
+	if pctx.WorkingDir != getContextValues("WorkingDir") {
+		t.Errorf("JSON Load - Context has wrong WorkingDir - Found %s, expected %s", pctx.WorkingDir, getContextValues("WorkingDir"))
+	}
+}
