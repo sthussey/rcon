23eb03161e390455171bd9e07fae13811f828412
Scott Hussey <sthussey@gmail.com>
2020-07-07 09:41:47 -0500

    Refactor to Go idiomatic structure
    - Refactor the layout to Go idiomatic structure
    - Fix a few issues in the cmd entrypoint

---
+++ cmd/rcon/rcon.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"github.com/sthussey/rcon/internal"
+	"os"
+)
+
+func main() {
+	configPath := flag.String("c", "", "Path to context config YAML or JSON file.")
+	rc := internal.NewRunConfig(*configPath)
+	err := rc.Run()
+
+	if err != nil {
+		fmt.Printf("Error running context: %v", err)
+		os.Exit(1)
+	}
+
+	os.Exit(0)
+}
